e83545643cae6d520438f9485f2de3e6c6f31b17
Ganesh N. Sivalingam <g.n.sivalingam@gmail.com>
2019-11-09 09:15:29 +0000

    Initial commit with first solution

---
+++ roman_to_integer.py
@@ -0,0 +1,34 @@
+#https://leetcode.com/problems/roman-to-integer/submissions/
+
+raw_values = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
+
+
+class Solution:
+    def sliding_window_generator(self, s: str):
+        for i in range(len(s) - 1):
+            yield (
+                raw_values[s[i]],
+                raw_values[s[i + 1]]
+            )
+        yield (raw_values[s[-1]], 0)
+
+    def romanToInt(self, s: str) -> int:
+        output = 0
+        modifier = 0
+
+        for a, b in self.sliding_window_generator(s):
+            if a < b:
+                modifier = a
+            else:
+                output += a - modifier
+                modifier = 0
+        return output
+
---
+++ test/test_roman_to_integer.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from roman_to_integer import Solution
+
+
+class TestSolution(TestCase):
+    def test_3(self):
+        self.assertEqual(3, Solution().romanToInt("III"))
+        print("done")
+
+    def test_4(self):
+        self.assertEqual(4, Solution().romanToInt("IV"))
+
+    def test_9(self):
+        self.assertEqual(9, Solution().romanToInt("IX"))
+
+    def test_58(self):
+        self.assertEqual(58, Solution().romanToInt("LVIII"))
+
+    def test_1994(self):
+        self.assertEqual(1994, Solution().romanToInt("MCMXCIV"))
+
+    def test_sliding_window_generator(self):
+        generator = Solution().sliding_window_generator("IVX")
+        output = list(generator)
+        self.assertListEqual([(1, 5), (5, 10), (10, 0)], output)
